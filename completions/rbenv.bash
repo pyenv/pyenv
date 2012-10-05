02332d78f38d9baef7374841d736331dc5eaff09
Brian Olsen <brian@maven-group.org>
2012-10-05 04:28:25 +0200

    Added bash support for completion of full command line

--- completions/rbenv.bash
+++ completions/rbenv.bash
@@ -5,8 +5,10 @@ _rbenv() {
   if [ "$COMP_CWORD" -eq 1 ]; then
     COMPREPLY=( $(compgen -W "$(rbenv commands)" -- "$word") )
   else
-    local command="${COMP_WORDS[1]}"
-    local completions="$(rbenv completions "$command")"
+    local words=("${COMP_WORDS[@]}")
+    unset words[0]
+    unset words[$COMP_CWORD]
+    local completions=$(rbenv completions "${words[@]}")
     COMPREPLY=( $(compgen -W "$completions" -- "$word") )
   fi
 }
