ffa2505450f77442a11d14fdc80031f47a649836
Yamashita Yuu <yamashita@geishatokyo.com>
2012-10-23 15:23:48 +0900

    add install script

---
+++ plugins/python-build/install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+
+if [ -z "${PREFIX}" ]; then
+  PREFIX="/usr/local"
+fi
+
+BIN_PATH="${PREFIX}/bin"
+SHARE_PATH="${PREFIX}/share/python-build"
+
+mkdir -p "${BIN_PATH}"
+mkdir -p "${SHARE_PATH}"
+
+for file in bin/*; do
+  cp "${file}" "${BIN_PATH}"
+done
+
+for file in share/python-build/*; do
+  cp "${file}" "${SHARE_PATH}"
+done
+
+echo "Installed python-build at ${PREFIX}"
