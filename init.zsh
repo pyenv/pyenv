e3e1743116e9510d86abdbe9636d7df2665b2604
Augusto F. Hack <hack.augusto@gmail.com>
2017-01-22 17:21:33 -0200

    lazy rehash & compsys completion

--- init.zsh
+++ init.zsh
@@ -1,8 +1,23 @@
-dir=$(dirname $0)
+root=$(dirname $0)
 
-for plugin in ${dir}/plugins/*; do
+for plugin in ${root}/plugins/*; do
     [ -d "${plugin}/bin" ] && export PATH="${plugin}/bin:$PATH"
 done
-export PATH="${dir}/bin:$PATH"
+
+export PATH="${root}/bin:$PATH"
+export PYENV_SHELL=zsh
+# fpath=($root/completions $fpath)
+
+# lazy pyenv init/rehash
+pyenv() {
+    unset -f pyenv
+
+    # make sure the shims are initiated and usable
+    mkdir -p "${PYENV_ROOT}/"{shims,versions}
+    export PATH="${PYENV_ROOT}/shims:${PATH}"
+    command pyenv rehash 2>/dev/null
+
+    ${root}/bin/pyenv "$@"
+}
 
 true # Return success exit code otherwise antigen breaks
