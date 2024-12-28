6112062fdd57d84330def5e6f8a5e1634a019fd0
Christian Fredrik Johnsen <christian@johnsen.no>
2024-12-28 12:41:09 +0100

    fix: add automatic rehash after pip3.x/pip3.xx install
    The tests showed that the original implementation actually never worked.
    You did NOT get a rehash after doing pip3.8 install, even when the
    comments in the code said so.
    
    --> replaced `\d` with [0-9] and slapped on a `+`, such that we match
    both pip3.x and `pip3.xx install`

--- pyenv.d/exec/pip-rehash.bash
+++ pyenv.d/exec/pip-rehash.bash
@@ -2,7 +2,7 @@ PYENV_PIP_REHASH_ROOT="${BASH_SOURCE[0]%/*}/pip-rehash"
 PYENV_REHASH_COMMAND="${PYENV_COMMAND##*/}"
 
 # Remove any version information, from e.g. "pip2" or "pip3.4".
-if [[ $PYENV_REHASH_COMMAND =~ ^(pip|easy_install)[23](\.\d)?$ ]]; then
+if [[ $PYENV_REHASH_COMMAND =~ ^(pip|easy_install)[23](\.[0-9]+)?$ ]]; then
   PYENV_REHASH_COMMAND="${BASH_REMATCH[1]}"
 fi
 
